a77948fd8d56cc99d686aa5d2f70c677e1391329
KingOfArthis <kingofarthis@gmail.com>
2023-01-06 09:19:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-var username;
-var password;
+var login = false;
+var frames = 0;
 
-function login()    {
-    windows.alert("Please Login to Continue!");
-    return;
+
+//Keeps track of the frames that have gone by
+function timeKeeping()  {
+    for (var i = 0; i >= 0; i++)   {
+
+        //counts up number of frames
+        frames = frames + 1;
+
+        //display the number of frames that have gone by in console
+        console.log(String(frames));
+    }
 }
 
-//login();
+function loginAlert()   {
+    if (login = false && frames >= 1000)   {
+        
+        //alerts user aproximately 10 seconds in
+        alert("Please login to continue");
+    }
+}
+
+timeKeeping();
+loginAlert();
